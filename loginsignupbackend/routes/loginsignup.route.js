6e090d1c21d42c345d7d65f83b9543a392310ad1
amangaur95 <43569264+amangaur95@users.noreply.github.com>
2018-10-18 15:07:32 +0530

    Update loginsignup.route.js

--- loginsignupbackend/routes/loginsignup.route.js
+++ loginsignupbackend/routes/loginsignup.route.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const config = require('../config/database');
-require('../config/passport')(passport);
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
